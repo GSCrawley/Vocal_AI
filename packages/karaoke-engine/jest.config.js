64e5bf874f51a587d8b158da9d305bb216446549
GSCrawley <31636555+GSCrawley@users.noreply.github.com>
2026-05-17 22:58:55 +0000

    Optimize computePitchSimilarity by pulling out Math.log2 logic from DTW distFunc

---
+++ packages/karaoke-engine/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.json' }]
+  },
+  moduleNameMapper: {
+    '^@voice/shared-types$': '<rootDir>/../shared-types/src/index.ts'
+  }
+};
