f86bedf1561e1ced7be1c4533d8506a235534eec
Gideon S Crawley <gidstube@gmail.com>
2026-05-23 15:15:59 -0700

    feat(karaoke-engine): add tsconfig.jest.json and update jest config to use it

--- packages/karaoke-engine/jest.config.js
+++ packages/karaoke-engine/jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   transform: {
-    '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.json' }]
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.jest.json' }]
   },
   moduleNameMapper: {
     '^@voice/shared-types$': '<rootDir>/../shared-types/src/index.ts'
