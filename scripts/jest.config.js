8c3cfeeb0a5027217ec63615bfa8edfcd875b526
GSCrawley <31636555+GSCrawley@users.noreply.github.com>
2026-05-31 19:18:20 +0000

    fix: format jest.config.js to satisfy prettier CI check

--- scripts/jest.config.js
+++ scripts/jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   transform: {
-    '^.+\\.tsx?$': ['ts-jest', { tsconfig: './scripts/tsconfig.test.json' }]
-  }
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: './scripts/tsconfig.test.json' }],
+  },
 };
