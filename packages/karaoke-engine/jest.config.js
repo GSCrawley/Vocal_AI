e99924fa4353be078664ef3253a7fe9a38bd07e1
GSCrawley <31636555+GSCrawley@users.noreply.github.com>
2026-06-24 02:12:46 +0000

    test(karaoke-engine): add tests for computeContourMatch

---
+++ packages/karaoke-engine/jest.config.js
@@ -0,0 +1,10 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@voice/shared-types$': '<rootDir>/../shared-types/src/index.ts',
+  },
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: './tsconfig.test.json' }]
+  }
+};
